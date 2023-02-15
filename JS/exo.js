505f0eceb2d10c48fbccde185bd9e936b4185e5c
Chaima Alauzet <alzt.chaima@gmail.com>
2023-02-15 19:40:57 +0100

    Ajout des 3 div suivantes

--- JS/exo.js
+++ JS/exo.js
@@ -1,2 +1,7 @@
 let elt = document.getElementById('main');
 elt.innerHTML = "<div>Je suis la div 1</div><div>Je suis la div 2</div><div>Je suis la div 3</div>";
+
+var newElm = document.createElement("div");
+newElm.innerHTML = "<div>Je suis la div 4</div><div>Je suis la div 5</div><div>Je suis la div 6</div>";
+elt.appendChild(newElm);
+
