c18ae35672a94ec7a30a9cf2faffb6a055d0fde0
Chaima Alauzet <alzt.chaima@gmail.com>
2023-03-16 08:26:28 +0100

    récupération du parametre name de l'URL

--- JS/exo.js
+++ JS/exo.js
@@ -21,3 +21,44 @@ function colorChange() {
 }
 
 div3.addEventListener("click", colorChange);
+
+const serpiere = { 
+    couleur: "vert d'eau", 
+    marque: "Ikea", 
+    prix: 12.99
+}
+
+localStorage.setItem("serpiere", JSON.stringify(serpiere));
+const serpiereLocalStorage = JSON.parse(localStorage.getItem("serpiere"));
+console.log(serpiereLocalStorage);
+
+const marque = serpiereLocalStorage.marque;
+
+const div2 = elt.getElementsByTagName("div")[1];
+div2.innerHTML = "La marque est " + marque;
+
+const nameInput = document.getElementById('name');
+const submitButton = document.getElementById('submitBtn');
+console.log(submitButton)
+submitButton.addEventListener("click", function(){
+    const name = nameInput.value;
+    localStorage.setItem("nom", name);
+});
+
+// Deuxième partie supprimer local storage en utilisant le bouton Display :
+const removeData = document.getElementById('displayBtn');
+console.log(removeData)
+const recup = document.getElementById('recup')
+removeData.addEventListener('click', function(){
+    localStorage.clear();
+})
+
+var str = "https://www.domain.com/page?name=francois1&id=opwiueru3920983r2&lastname=john"
+var url = new URL(str);
+
+var search_params = new URLSearchParams(url.search);
+
+if (search_params.has('name')){
+    var name = search_params.get('name');
+    console.log(name)
+}
